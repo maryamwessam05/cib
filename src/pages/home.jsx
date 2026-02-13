26524a7577305c58ac83204993750c7a0cb72809
zeina <23-101312@students.eui.edu.eg>
2026-02-13 18:32:23 +0200

    section5

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,9 @@ import Hero from '../compnents/hero/hero';
 import Section2 from '../compnents/section2/section2';
 import Section3 from '../compnents/section3/section3';
 import Section4 from '../compnents/section4/section4';
+import img3 from '../assets/img3.png'
+import './home.css'
+import Bluebtn from '../compnents/bluebtn/bluebtn';
 
 const Home = () => {
     return ( 
@@ -14,6 +17,16 @@ const Home = () => {
         <Section2 />
         <Section3 />
         <Section4 />
+
+        <section className='section5'>
+            <div className='sec5_inside'>
+                <div className='sec5_left'>
+                    <h2 className='sec5_h1'>CIB on an international scale</h2>
+                    <p className='sec5text'>Learn more about CIB's presence outside of Egypt</p>
+                    <Bluebtn />
+            </div>
+            </div>
+        </section>
         
         </>
      );
